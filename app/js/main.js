c7cbaf909f973ecb79f0fafcbea74295e9799741
Anton <kodvmeshke@gmail.com>
2022-07-10 20:41:26 +0300

    Part 3

--- app/js/main.js
+++ app/js/main.js
@@ -28,6 +28,13 @@ $('.slider-map').slick({
   arrows: false,
   asNavFor: '.surf-slider',
   focusOnSelect: true,
+});
+
+$('.travel__slider').slick({
+  infinite: true,
+  fade: true,
+  prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+  nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
 })
 
 });
